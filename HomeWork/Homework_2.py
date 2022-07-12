d88629494c8ac44fdeb01a5bbb65b6bf9722a932
Helen <e.xolodkova@inbox.ru>
2022-07-12 23:57:02 +0300

    Добавили варианты решения задачи №1

--- HomeWork/Homework_2.py
+++ HomeWork/Homework_2.py
@@ -1,30 +1,45 @@
 # Задача №1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-def summary(num: int):
-    result = 0
-    while num > 0:
-        result += num % 10
-        num = num // 10
-    return result
+# 1й вариант,только для целых чисел
+# def summary(num: int):
+#     result = 0
+#     while num > 0:
+#         result += num % 10
+#         num = num // 10
+#     return result
 
-print(summary(6782))
+# n = int(input('Введите целое число n: '))
+# print('Сумма цифр числа равна {}'.format(summary(n)))
 
-# # 2й вариант
-print(sum(map(int, str(6782))))
+# 2й вариант(универсальный)
+# def sumnum (n):
+#     res = 0
+#     for num in str(n):
+#         if num == '.' or num == ',':
+#             continue
+#         res = res + int(num)
+#     return res
+
+# number = input('Введите число: ')
+# print(f'Сумма цифр числа {number} равна {sumnum(number)}')
+
+
+# 2й вариант, в одну строку
+# print(sum(map(int, str(6782))))
 
 
 
 # Задача №2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
-def factorial(n: int) -> int:
-    if n == 1:
-        return 1
-    else: 
-        return n * factorial(n - 1)
+# def factorial(n: int) -> int:
+#     if n == 1:
+#         return 1
+#     else: 
+#         return n * factorial(n - 1)
 
-n = 4
-for i in range(1, n+1):
-    print(factorial(i), end=' ')
+# n = 4
+# for i in range(1, n+1):
+#     print(factorial(i), end=' ')
 
 
 
